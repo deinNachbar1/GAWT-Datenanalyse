5c16cac08c6be2ecfece4317e78a2ef5b001e5eb
deinNachbar1 <74561856+deinnachbar1@users.noreply.github.com>
2024-01-17 19:54:43 +0100

    Update datensatz_auswertung.py
    Kovarianz hinzugefügt

--- Code/datensatz_auswertung.py
+++ Code/datensatz_auswertung.py
@@ -1,5 +1,60 @@
 import csv
 import math
+import os
+
+
+def main():
+    print("dateiname: ", end="")
+    filename = input()
+
+    if os.path.isfile(filename):
+        print()
+
+        with open(filename, "r") as file:
+            reader = csv.reader(file, delimiter=';')
+
+            x = ""
+            y = ""
+
+            werteX = []
+            werteY = []
+            for row in reader:
+
+                if reader.line_num == 1:
+                    print(row[0] + " | " + row[1])
+                    print("------------------")
+                    x = row[0]
+                    y = row[1]
+                else:
+                    print(row[0] + " | " + row[1])
+                    werteX.append(float(row[0].replace(",", ".")))
+                    werteY.append(float(row[1].replace(",", ".")))
+
+            print_rangliste(x, werteX)
+            print_rangliste(y, werteY)
+
+            print()
+
+            print_median(x, werteX)
+            print_median(y, werteY)
+
+            print()
+
+            print_arithmetische_mittel(x, werteX, 3)
+            print_arithmetische_mittel(y, werteY, 3)
+
+            print()
+
+            print_spannweite(x, werteX, 3)
+            print_spannweite(y, werteY, 3)
+
+            print()
+
+            print_kovarianz(werteX, werteY, 3)
+
+    else:
+        print("datei existiert nicht!\n")
+        main()
 
 
 def print_rangliste(name, werte):
@@ -44,47 +99,26 @@ def print_spannweite(name, werte, nachkommastelle):
     print("Spannweite " + name + ": " + str(spannweite))
 
 
-def main():
-    print("dateiname: ")
-    filename = input()
+def print_kovarianz(werte1, werte2, nachkommastelle):
+    mittelwert1 = 0
+    mittelwert2 = 0
 
-    with open(filename, "r") as file:
-        reader = csv.reader(file, delimiter=';')
+    for i in werte1:
+        mittelwert1 += i
+    for i in werte2:
+        mittelwert2 += i
 
-        x = ""
-        y = ""
+    mittelwert1 = mittelwert1 / len(werte1)
+    mittelwert2 = mittelwert2 / len(werte2)
 
-        werteX = []
-        werteY = []
-        for row in reader:
+    kovarianz = 0
 
-            if reader.line_num == 1:
-                print(row[0] + " | " + row[1])
-                print("------------------")
-                x = row[0]
-                y = row[1]
-            else:
-                print(row[0] + " | " + row[1])
-                werteX.append(float(row[0].replace(",", ".")))
-                werteY.append(float(row[1].replace(",", ".")))
+    for i in range(0, len(werte1)):
+        kovarianz += (werte1[i] - mittelwert1) * (werte2[i] - mittelwert2)
 
-        print_rangliste(x, werteX)
-        print_rangliste(y, werteY)
+    kovarianz = kovarianz / len(werte2) - 1
 
-        print()
-
-        print_median(x, werteX)
-        print_median(y, werteY)
-
-        print()
-
-        print_arithmetische_mittel(x, werteX, 3)
-        print_arithmetische_mittel(y, werteY, 3)
-
-        print()
-
-        print_spannweite(x, werteX, 3)
-        print_spannweite(y, werteY, 3)
+    print("Kovarianz: " + str(round(kovarianz, nachkommastelle)))
 
 
 if __name__ == "__main__":
